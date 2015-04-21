1e8234241fbc515b6bc9a9cb2c56608605f4ee67
Owen Brotherwood <ob@sjn01351.jnxtrmain00.corp.jndata.net>
2015-04-21 12:16:55 +0200

    hopes for AAA as a part of boot js scripts as a tidy place for general AAA

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,3 +1,6 @@
+/*
+ * Can this authentication.js take care of some more authentication tasks  ...?
+ */
 module.exports = function enableAuthentication(server) {
   // enable authentication
   server.enableAuth();
---
+++ server/boot/authorization.js
@@ -0,0 +1,3 @@
+/*
+ * It would be nice for some authorization.js to make AAA a part of boot
+ */
