75d5061d882f4dfcc8b33d65a39f584a0164f8a8
Owen Brotherwood <owen@brotherwood.biz>
2015-04-21 23:04:53 +0200

    server and token cookie questions

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,9 +1,18 @@
 /*
  * Can this authentication.js take care of some more authentication tasks  ...?
  * http://docs.strongloop.com/display/public/LB/Making+authenticated+requests
- * 
+ * http://docs.strongloop.com/display/public/LB/Working+with+LoopBack+objects
+ * http://docs.strongloop.com/display/public/LB/Working+with+LoopBack+objects#WorkingwithLoopBackobjects-Fromabootscript
  */
-module.exports = function enableAuthentication(server) {
+ /*
+* - where does server come from, can only find ref to app in boot
+* - can one get server from app and change to:
+* module.exports = funcion(app){
+*   app.something.server.enableAuth();
+*   app.use(loopback.token({ model: app.models.accessToken }));
+* }
+*/
+module.exports = function enableAuthentication(server) { 
   // enable authentication
   server.enableAuth();
 };
