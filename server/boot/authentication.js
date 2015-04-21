cce17cba8080c0e452cc68bc127c7afb9566bbfc
Owen Brotherwood <owen@brotherwood.biz>
2015-04-21 22:55:05 +0200

    more doc links

--- server/boot/authentication.js
+++ server/boot/authentication.js
@@ -1,5 +1,7 @@
 /*
  * Can this authentication.js take care of some more authentication tasks  ...?
+ * http://docs.strongloop.com/display/public/LB/Making+authenticated+requests
+ * 
  */
 module.exports = function enableAuthentication(server) {
   // enable authentication
--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
+/*
+* Play time for Owen Brotherwood, DK 2015
+*
+/
+
+// http://docs.strongloop.com/display/public/LB/config.json
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
 var app = module.exports = loopback();
 
+// http://docs.strongloop.com/display/public/LB/Defining+middleware
 // needed for middleware: I would love to not have these 2 lines in server.js
 var bodyParser = require('body-parser');
 app.middleware('initial', bodyParser.urlencoded({ extended: true }));
@@ -14,9 +20,10 @@ app.start = function() {
   });
 };
 
+// http://docs.strongloop.com/display/public/LB/Defining+boot+scripts
 boot(app, __dirname, function(err) {
   if (err) throw err;
   
-  if (require.main === module)
+  if (require.main === module) // use case for when one would not app.start()?
     app.start();
 });
