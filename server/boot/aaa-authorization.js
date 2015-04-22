fb7bb7124315c379b79f587f2c54a8ffd8772fa9
Owen Brotherwood <ob@sjn01351.jnxtrmain00.corp.jndata.net>
2015-04-23 00:52:02 +0200

    this and that

--- server/boot/aaa-authorization.js
+++ server/boot/aaa-authorization.js
@@ -1,41 +1,16 @@
 module.exports = function(app) {
-  var Role = app.models.Role;
-
-  Role.registerResolver('teamMember', function(role, context, cb) {
-    function reject() {
-      process.nextTick(function() {
-        cb(null, false);
-      });
-    }
-
-    // if the target model is not project
-    if (context.modelName !== 'project') {
-      return reject();
-    }
-
-    // do not allow anonymous users
-    var userId = context.accessToken.userId;
-    if (!userId) {
-      return reject();
-    }
-
-    // check if userId is in team table for the given project id
-    context.model.findById(context.modelId, function(err, project) {
-      if (err || !project)
-        return reject();
-
-      var Team = app.models.Team;
-      Team.count({
-        ownerId: project.ownerId,
-        memberId: userId
-      }, function(err, count) {
-        if (err) {
-          console.log(err);
-          return cb(null, false);
-        }
-
-        cb(null, count > 0); // true = is a team member
-      });
-    });
-  });
+/* 
+ * Define the basic social (facebook etc)  and internal groups (LDAP MS AD)
+ * 
+ */
+/*
+ * Candidate groups that should be present are Well-know goup
+ * Everyone                                                               Well-known group S-1-1-0
+ * NT AUTHORITY\REMOTE INTERACTIVE LOGON                                  Well-known group S-1-5-14
+ * NT AUTHORITY\INTERACTIVE                                               Well-known group S-1-5-4
+ * NT AUTHORITY\Authenticated Users                                       Well-known group S-1-5-11
+ * NT AUTHORITY\This Organization                                         Well-known group S-1-5-15
+ * LOCAL                                                                  Well-known group S-1-2-0
+ */
 }
+
